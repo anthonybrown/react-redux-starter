ff8f8c7cbde9af295572149ecb84120612f858e4
Tony Brown <tony@tonybrown.me>
2016-06-12 05:42:32 -0400

    setup tests using Test Utils

--- src/components/about/AboutPage.js
+++ src/components/about/AboutPage.js
@@ -6,17 +6,17 @@ class AboutPage extends React.Component {
 			<div className='container top'>
 				<h1>About</h1>
 				<div className='col-lg-8 col-md-8 col-sm-10'>
-					<p>This application uses React, Redux, React Router and a variety of other helpful libraries.</p>
-					<p>This app is written in ES6 syntax and uses both, class based components and stateless functional components</p>
+					<p>This application uses React, Redux, React Router and a variety of other helpful libraries for a responsive user experience.</p>
+					<p>This app is written in the ES6 syntax and uses both, class based components and stateless functional components.</p>
 					<p>
-						Basically this is the same app I built using ES5 syntax and the React.createClass({}) but now,
-						I'm using ES6, some testing and a Node/Express server.
-						Not much testing is going on and I'm just learning to be more comfortable using the new syntax in general.
-						I'm really liking it but it takes a little getting used to.
+						Basically this is the same app I built using the ES5 syntax with the React.createClass({}) but now,
+						I'm using ES6 syntax, with some testing and a Node/Express server.
+						I'm just getting use to the new ES6 syntax and the style used with React and Redux.
+						I'm really liking it but it takes a little getting used to but that will resolve itself with lots of practice.
 					</p>
 					<p>
 						The app just lets you view Pluralsight courses and they're authors and let's the user
-						perform basic CRUD actions, crete, retrive, update and delete. That's what it's going
+						perform basic CRUD actions, create, retrive, update and delete. That's what it's going
 						to do now but I'm going to making some changes to the API for something a little more useful
 						and or more features.
 					</p>
--- src/components/course/CourseForm.js
+++ src/components/course/CourseForm.js
@@ -4,42 +4,40 @@ import SelectInput from '../common/SelectInput';
 
 const CourseForm = ({course, allAuthors, onSave, onChange, saving, errors}) => {
 	return (
-		<div className='col-lg-6 col-md-8 col-sm-8 col-xs-10'>
-			<form>
-				<h1>Manage Course</h1>
-				<TextInput
-					name='title'
-					label='Title'
-					value={course.title}
-					onChange={onChange}
-					error={errors.title} />
-				<SelectInput
-					name='authorId'
-					label='Author'
-					value={course.authorId}
-					defaultOption='Select Author'
-					options={allAuthors}
-					onChange={onChange} error={errors.authorId} />
-				<TextInput
-					name='category'
-					label='Category'
-					value={course.category}
-					onChange={onChange}
-					error={errors.category} />
-				<TextInput
-					name='length'
-					label='Length'
-					value={course.length}
-					onChange={onChange}
-					error={errors.length} />
-				<input
-					type='submit'
-					disabled={saving}
-					value={saving ? 'Saving...' : 'Save'}
-					className='btn btn-primary'
-					onClick={onSave} />
-			</form>
-		</div>
+		<form className='col-lg-6 col-md-8 col-sm-8 col-xs-10'>
+			<h1>Manage Course</h1>
+			<TextInput
+				name='title'
+				label='Title'
+				value={course.title}
+				onChange={onChange}
+				error={errors.title} />
+			<SelectInput
+				name='authorId'
+				label='Author'
+				value={course.authorId}
+				defaultOption='Select Author'
+				options={allAuthors}
+				onChange={onChange} error={errors.authorId} />
+			<TextInput
+				name='category'
+				label='Category'
+				value={course.category}
+				onChange={onChange}
+				error={errors.category} />
+			<TextInput
+				name='length'
+				label='Length'
+				value={course.length}
+				onChange={onChange}
+				error={errors.length} />
+			<input
+				type='submit'
+				disabled={saving}
+				value={saving ? 'Saving...' : 'Save'}
+				className='btn btn-primary'
+				onClick={onSave} />
+		</form>
 	);
 };
 
